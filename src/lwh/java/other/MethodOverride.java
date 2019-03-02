4936a7479d157df0b26ad5ff607fabf6511fb53c
lwh <2391519868@qq.com>
2019-03-02 13:20:13 +0800

    方法重载

---
+++ src/lwh/java/other/MethodOverride.java
@@ -0,0 +1,17 @@
+package lwh.java.other;
+
+/**
+ * @author lwh
+ * @date 2019-03-02
+ * @desp
+ */
+public class MethodOverride {
+
+    public void sayHello(int a, float b){
+
+    }
+
+    public void sayHello(float a, int b){
+
+    }
+}
